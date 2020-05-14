494600692dee0e71fd8c723078c2c656e1aaed72
Matias Lindgren <matias.lindgren@gmail.com>
2020-05-15 00:00:51 +0300

    simplify lidbox-run wrapper

--- scripts/lidbox-run.bash
+++ scripts/lidbox-run.bash
@@ -1,11 +1,4 @@
 #!/usr/bin/env bash
 # Executable wrapper for sbatch on Slurm, this has no other purpose
 set -ue
-subcommand=$1
-config=$2
-if [ $# -gt 2 ]; then
-    kwargs="${@:3:$#}"
-else
-    kwargs='-v'
-fi
-lidbox $subcommand $config $kwargs
+lidbox $@
